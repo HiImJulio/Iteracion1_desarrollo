c38ae0dbf84458157a7c68b55658418ed94ad051
Alejandro Meza <56438011+jalex3421@users.noreply.github.com>
2020-08-02 18:55:22 +0200

    Update EdificioV1.py

--- EdificioV1.py
+++ EdificioV1.py
@@ -9,8 +9,6 @@ Autores:   Manuel López Amo-Ocón
            Alejangro Galván Pérez-Ilzarbe
            Santiago Cebellán
            Alejandro Meza Tudela
-           
-Fecha de versión final: 01-08-2020 
 '''
 
 
